bc538b8670080bd7265b8fa162e8df5db50656fd
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-22 15:58:10 +0200

    Create 4.3.1.7Lab.py

---
+++ 4.3.1.7Lab.py
@@ -0,0 +1,83 @@
+'''
+Estimated time
+15-20 minutes
+
+Level of difficulty
+Medium
+
+Prerequisites
+LAB 4.3.1.6
+
+Objectives
+Familiarize the student with:
+
+projecting and writing parameterized functions;
+utilizing the return statement;
+utilizing the student's own functions.
+Scenario
+Your task is to write and test a function which takes two arguments (a year and a month) and returns the number of days for the given month/year pair (while only February is sensitive to the year value, your function should be universal).
+
+The initial part of the function is ready. Now, convince the function to return None if its arguments don't make sense.
+
+Of course, you can (and should) use the previously written and tested function (LAB 4.3.1.6). It may be very helpful. We encourage you to use a list filled with the months' lengths. You can create it inside the function - this trick will significantly shorten the code.
+
+We've prepared a testing code. Expand it to include more test cases.
+
+def is_year_leap(year):
+#
+# Your code from LAB 4.3.1.6.
+#
+
+def days_in_month(year, month):
+#
+# Write your new code here.
+#
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr, mo, "->", end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
+
+'''
+
+# solution 
+
+def is_year_leap(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+
+def days_in_month(year, month):
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month < 1 or month > 12:
+        return None
+    elif month == 2 and is_year_leap(year):
+        return 29
+    else:
+        return months[month-1]
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr, mo, "->", end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
